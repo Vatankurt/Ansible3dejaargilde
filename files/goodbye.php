55c860149d76b50e32bfda503f0ab850650bc4b4
Vatankurt <vatankurt2003@gmail.com>
2023-09-15 12:24:43 +0000

    definitive versie

---
+++ files/goodbye.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Goodbye PHP Page</title>
+</head>
+<body>
+    <h1>Goodbye, World! This is PHP Page 2</h1>
+    <?php
+        echo "This is another PHP page.";
+    ?>
+</body>
+</html>
---
+++ files/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello PHP Page</title>
+</head>
+<body>
+    <h1>Hello, World! This is PHP Page 1</h1>
+    <?php
+        echo "This is a simple PHP page.";
+    ?>
+</body>
+</html>
